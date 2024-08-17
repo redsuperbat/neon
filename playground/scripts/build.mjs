fe5fc4a5db8fe39e1fa94f35f3ead243bc7f596e
Max Netterberg <max.netterberg@gmail.com>
2024-08-17 20:32:56 +0200

    remove gh from build step

--- playground/scripts/build.mjs
+++ playground/scripts/build.mjs
@@ -1,16 +1,8 @@
-import { within, $, cd, fs } from "zx";
-import pages from "gh-pages";
+import { within, $, cd } from "zx";
 
 within(async () => {
   cd("../crates/neon_web");
   await $`wasm-pack build --target web`;
 });
 
-await $`tsc -b && vite build`;
-
-await fs.stat("dist");
-await new Promise((res) =>
-  pages.publish("dist", () => {
-    res();
-  }),
-);
+await $`tsc -b && vite build --base /neon`;
