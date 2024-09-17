708f2a14d9afd259bcc74e02e2f0c46e0944678a
Max Netterberg <max.netterberg@gmail.com>
2024-09-17 17:22:02 +0200

    Updates

--- assets/javascript-DnLO3keP.js
+++ assets/javascript-DnLO3keP.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-j-FXOzVt.js";import"./index-Dbbur3CH.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-BIBaZwL-.js";import"./index-DYu8NwaC.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.51.0(67d664a32968e19e2eb08b696a92463804182ae4)
  * Released under the MIT license
--- assets/tsMode-DKUvWQp5.js
+++ assets/tsMode-DKUvWQp5.js
@@ -1,4 +1,4 @@
-import{t as O,m as I}from"./index-Dbbur3CH.js";/*!-----------------------------------------------------------------------------
+import{t as O,m as I}from"./index-DYu8NwaC.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.51.0(67d664a32968e19e2eb08b696a92463804182ae4)
  * Released under the MIT license
