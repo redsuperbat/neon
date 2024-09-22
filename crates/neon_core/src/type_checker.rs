f099a092815e712c968c625c3f3b99f7f77fab7c
Max Netterberg <max.netterberg@gmail.com>
2024-09-22 18:59:31 +0200

    add more type checking

--- crates/neon_core/src/type_checker.rs
+++ crates/neon_core/src/type_checker.rs
@@ -1,5 +1,5 @@
 use core::mem::discriminant as tag;
-use std::{collections::HashMap, fmt::Display};
+use std::{borrow::BorrowMut, collections::HashMap, fmt::Display};
 
 use crate::{
     diagnostic::{
@@ -30,37 +30,49 @@ pub struct ArrayType {
 }
 
 #[derive(Debug, Clone)]
-pub struct UnionType {
-    variants: Vec<Type>,
-}
-
-#[derive(Debug, Clone)]
 pub enum Type {
     Never,
     Int,
     Bool,
     Object(ObjectType),
     Array(ArrayType),
-    Union(UnionType),
-    Any,
+    TypeFn,
+
     String,
     Unit,
-    TypeFn,
+    Any,
 }
 
 impl Display for Type {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let name = match self {
-            Type::String => "string".to_string(),
-            Type::Int => "int".to_string(),
-            Type::Bool => "boolean".to_string(),
-            Type::Unit => "unit".to_string(),
-            Type::Never => "never".to_string(),
-            Type::Any => "any".to_string(),
-            Type::TypeFn => "fn".to_string(),
-            t => todo!("{:?}", t),
-        };
-        write!(f, "{name}")
+        fn fmt_type(depth: usize, t: &Type) -> String {
+            match t {
+                Type::String => "string".to_string(),
+                Type::Int => "int".to_string(),
+                Type::Bool => "boolean".to_string(),
+                Type::Unit => "unit".to_string(),
+                Type::Never => "never".to_string(),
+                Type::Any => "any".to_string(),
+                Type::TypeFn => "fn".to_string(),
+                Type::Array(t) => format!("{}[]", t.elements),
+                Type::Object(ObjectType { properties }) => {
+                    let mut result = String::from("{\n");
+                    let indentation = " ".repeat(depth);
+
+                    for (i, PropertyType { name, value }) in properties.iter().enumerate() {
+                        if i != 0 {
+                            result += ",\n"
+                        }
+                        result += &format!("{}{name}: {}", indentation, fmt_type(depth + 1, value));
+                    }
+
+                    result += &format!("\n{indentation}}}");
+                    result
+                }
+            }
+        }
+
+        write!(f, "{}", fmt_type(0, self))
     }
 }
 
@@ -126,14 +138,16 @@ impl TypeChecker {
 
     fn typeof_property_access(
         &mut self,
-        node: &PropertyAccessNode,
-        env: &mut TypeEnvironment,
+        _node: &PropertyAccessNode,
+        _env: &mut TypeEnvironment,
     ) -> Type {
         Type::Any
     }
 
-    fn typeof_array(&mut self, node: &ArrayNode, env: &mut TypeEnvironment) -> Type {
-        Type::Any
+    fn typeof_array(&mut self, _node: &ArrayNode, _env: &mut TypeEnvironment) -> Type {
+        Type::Array(ArrayType {
+            elements: Box::new(Type::Any),
+        })
     }
 
     fn typeof_else(&mut self, node: &ElseNode, env: &mut TypeEnvironment) -> Type {
@@ -142,7 +156,7 @@ impl TypeChecker {
 
     fn typeof_for_loop(&mut self, node: &ForLoopNode, env: &mut TypeEnvironment) -> Type {
         self.typeof_expression(&node.body, env);
-        Type::Any
+        Type::Unit
     }
 
     fn typeof_fn(&mut self, node: &FnNode, env: &mut TypeEnvironment) -> Type {
@@ -151,18 +165,20 @@ impl TypeChecker {
             self.typeof_identifier(param, env);
         }
         self.typeof_expression(&node.body, env);
-        Type::Any
+        env.bindings.insert(node.name.clone(), Type::TypeFn);
+        Type::TypeFn
     }
 
     fn typeof_invocation(&mut self, node: &InvocationNode, env: &mut TypeEnvironment) -> Type {
         let callee = self.typeof_expression(&node.callee, env);
-        return self.unify(callee, Type::TypeFn);
+        println!("callee, {}", callee);
+        return self.unify(&callee, &Type::TypeFn).clone();
     }
 
     fn typeof_binary(&mut self, node: &BinaryOperationNode, env: &mut TypeEnvironment) -> Type {
         let lhs = self.typeof_expression(&node.left, env);
         let rhs = self.typeof_expression(&node.right, env);
-        self.unify(lhs, rhs)
+        self.unify(&lhs, &rhs)
     }
 
     fn typeof_block(&mut self, node: &BlockNode, env: &mut TypeEnvironment) -> Type {
@@ -193,14 +209,15 @@ impl TypeChecker {
     fn typeof_assignment(&mut self, node: &AssignmentNode, env: &mut TypeEnvironment) -> Type {
         let lhs = self.typeof_identifier(&node.identifier, env);
         let rhs = self.typeof_expression(&node.right, env);
-        self.unify(lhs, rhs);
+        self.unify(&lhs, &rhs);
         Type::Unit
     }
 
     fn typeof_if(&mut self, node: &IfNode, env: &mut TypeEnvironment) -> Type {
-        // TODO: We should do a better check here to make sure that the predicate resolves to a
-        // boolean type
-        self.typeof_expression(&node.predicate, env);
+        let predicate = self.typeof_expression(&node.predicate, env);
+        println!("predicate {}", predicate);
+        println!("{:#?}", node.predicate);
+        self.unify(&Type::Bool, &predicate);
 
         let Some(alternate) = node.alternate.as_ref() else {
             return Type::Unit;
@@ -210,7 +227,6 @@ impl TypeChecker {
         let alternate = self.typeof_expression(&alternate, env);
 
         if consequent != alternate {
-            println!("{},{}, {:#?}", consequent, alternate, node);
             return self.add_error_diagnostic(ErrorDiagnostic::IncompatibleTypes(
                 IncompatibleTypesError {
                     loc: node.loc,
@@ -234,38 +250,56 @@ impl TypeChecker {
         Type::Never
     }
 
-    fn type_error(&mut self, lhs: Type, rhs: Type) -> Type {
+    fn type_error(&mut self, lhs: &Type, rhs: &Type) -> Type {
         self.add_error_diagnostic(ErrorDiagnostic::UnassignableType(UnassignableTypeError {
             loc: self.current_loc,
-            lhs,
-            rhs,
+            lhs: lhs.clone(),
+            rhs: rhs.clone(),
         }));
         Type::Never
     }
 
-    fn unify(&mut self, lhs: Type, rhs: Type) -> Type {
-        if rhs == Type::Any || lhs == Type::Any {
+    fn unify(&mut self, lhs: &Type, rhs: &Type) -> Type {
+        if *rhs == Type::Any || *lhs == Type::Any {
             return Type::Any;
         }
 
-        match lhs {
-            Type::Int => match rhs {
+        match &lhs {
+            Type::Int => match &rhs {
                 Type::Int => Type::Int,
-                _ => self.type_error(lhs, rhs),
+                _ => self.type_error(&lhs, rhs),
             },
-            Type::Bool => match rhs {
+            Type::Bool => match &rhs {
                 Type::Bool => Type::Bool,
                 _ => self.type_error(lhs, rhs),
             },
-            Type::String => match rhs {
+            Type::String => match &rhs {
                 Type::String => Type::String,
                 _ => self.type_error(lhs, rhs),
             },
+            Type::Unit => match &rhs {
+                Type::Unit => Type::Unit,
+                _ => self.type_error(lhs, rhs),
+            },
+            Type::Object(a) => match &rhs {
+                Type::Object(b) => {
+                    for PropertyType { name, value } in &a.properties {
+                        let b_prop = b.properties.iter().find(|p| p.name == *name);
+                        let Some(b_prop) = b_prop else {
+                            return self.type_error(lhs, rhs);
+                        };
+                        self.unify(value, &b_prop.value);
+                    }
 
-            Type::Object(_) => todo!(),
-            Type::Array(_) => todo!(),
-            Type::Union(_) => todo!(),
-            Type::Unit => todo!(),
+                    lhs.clone()
+                }
+
+                _ => self.type_error(lhs, rhs),
+            },
+            Type::Array(a) => match &rhs {
+                Type::Array(b) => self.unify(&a.elements, &b.elements),
+                _ => self.type_error(lhs, rhs),
+            },
             _ => Type::Never,
         }
     }
