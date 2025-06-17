41d3e396f3d9934da2ce6f357e9dc6244c5be22c
Max Netterberg <max.netterberg@gmail.com>
2025-06-17 21:02:53 +0200

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  name = "neon";
+
+  buildInputs = [
+    pkgs.rustc
+    pkgs.cargo
+    pkgs.rustfmt
+    pkgs.clippy
+    pkgs.cargo-watch
+    pkgs.rust-analyzer
+    pkgs.nodejs
+    pkgs.yarn
+  ];
+}
